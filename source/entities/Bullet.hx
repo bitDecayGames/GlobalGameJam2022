c76ddf82852eae87d9563ab064a0b20db096156b
mwingfield <mwingfie@gmail.com>
2022-01-29 17:51:38 -0700

    bigger bullet

--- source/entities/Bullet.hx
+++ source/entities/Bullet.hx
@@ -9,21 +9,24 @@ import flixel.util.FlxColor;
 using echo.FlxEcho;
 
 class Bullet extends FlxSprite {
+	public static final BULLET_RADIUS:Int = 10;
+
 	public var isLethal:Bool = true;
-    var maxBounces = 1;
-    var remainingBounces = 1;
+
+	var maxBounces = 1;
+	var remainingBounces = 1;
 
 	public function new(x:Float, y:Float, vel:FlxPoint) {
 		super(x, y);
-		makeGraphic(10, 10, FlxColor.RED);
+		makeGraphic(BULLET_RADIUS * 2, BULLET_RADIUS * 2, FlxColor.RED);
 
 		this.add_body({
-			x: x + 5,
-			y: y + 5,
+			x: x + BULLET_RADIUS,
+			y: y + BULLET_RADIUS,
 			elasticity: 0.95,
 			shape: {
 				type: CIRCLE,
-				radius: 5
+				radius: BULLET_RADIUS
 			},
 			velocity_x: vel.x,
 			velocity_y: vel.y
@@ -36,11 +39,11 @@ class Bullet extends FlxSprite {
 		this.get_body().active = false;
 	}
 
-    public function hit(terrain:FlxSprite, collisionData:Array<CollisionData>) {
-        // override this to do cool stuff on a per-bullet type basis
-        remainingBounces--;
-        if (remainingBounces < 0) {
-            kill();
-        }
-    }
+	public function hit(terrain:FlxSprite, collisionData:Array<CollisionData>) {
+		// override this to do cool stuff on a per-bullet type basis
+		remainingBounces--;
+		if (remainingBounces < 0) {
+			kill();
+		}
+	}
 }
