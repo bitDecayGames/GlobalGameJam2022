f6e591007ed0ce932f6697f9d70be0a9e4bc365d
Tristan Havelick <tristan@havelick.com>
2022-01-29 20:54:06 -0700

    cammera follow logic kinda works but it's really off

--- source/entities/CameraManager.hx
+++ source/entities/CameraManager.hx
@@ -23,11 +23,15 @@ class CameraManager {
     var oldScrollY:Float = 0;
     var currentTransition:Transition = Transition.None;
     var currentTarget:FlxObject;
+    var baseCameraXOffset:Float;
+    var baseCameraYOffset:Float;
 
     public function new(camera:FlxZoomCamera, focusHome:FlxObject) {
         this.camera = camera;
         this.focusHome = focusHome;
         this.currentTarget = focusHome;
+        this.baseCameraXOffset = -(camera.scroll.x - focusHome.x);
+        this.baseCameraYOffset = -(camera.scroll.y - focusHome.y);
     }
 
     public function zoomTo(player:Player) {
@@ -36,9 +40,10 @@ class CameraManager {
         //this.camera.zoomSpeed = 0.5;
         this.currentTarget = player;
         this.camera.follow(player, FlxCameraFollowStyle.LOCKON, 0.02);
-    }
+    };
 
     public function zoomOut() {
+        trace("zoom out");
         this.currentTransition = Transition.ZoomOut;
         this.currentTarget = focusHome;
         this.camera.targetZoom = 0;
@@ -48,18 +53,20 @@ class CameraManager {
     private function isCameraOnTarget() {
         var closeEnoguh = 10;
 
-        // These offset calculations don't work. Maybe something from the traces below will help, but I can't make sense of it
-        var xOffset = Math.abs(camera.scroll.x + FlxG.width / 2 + currentTarget.x );
-        var yOffset = Math.abs(camera.scroll.y + FlxG.height / 2 + currentTarget.y );
+        var xOffset = Math.abs(camera.scroll.x + FlxG.width / 2 + this.baseCameraXOffset - currentTarget.getMidpoint().x);
+        var yOffset = Math.abs(camera.scroll.y + FlxG.height / 2 + this.baseCameraYOffset - currentTarget.getMidpoint().y);
 
-        trace("camera scroll:" + camera.scroll.x + ", " + camera.scroll.y);
-        trace("camera scroll - screen w/h:" + (camera.scroll.x - FlxG.width) + ", " + (camera.scroll.y - FlxG.height));
-        trace("target midpoint: " + currentTarget.getMidpoint().x, currentTarget.getMidpoint().y);
-        trace("target x, y: " + currentTarget.x + ", " + currentTarget.y);
-        trace("target w x h: " + currentTarget.width + ", " + currentTarget.height);
-        trace("targetOffset:" + camera.targetOffset.x + ", " + camera.targetOffset.y);
+        // trace("camera scroll:" + camera.scroll.x + ", " + camera.scroll.y);
+        // trace("camera scroll - screen w/h:" + (camera.scroll.x - FlxG.width) + ", " + (camera.scroll.y - FlxG.height));
+        // trace("target midpoint: " + currentTarget.getMidpoint().x, currentTarget.getMidpoint().y);
+        // trace("target x, y: " + currentTarget.x + ", " + currentTarget.y);
+        // trace("target w x h: " + currentTarget.width + ", " + currentTarget.height);
+        // trace("FlxG wxh:" + FlxG.width + ", " + FlxG.height);
         trace("distance to target?: " + xOffset + ", " + yOffset);
-        return xOffset < closeEnoguh && yOffset < closeEnoguh; 
+        
+        // The y coord seems off by 64 pixels even though it's calculated the same as x, but the way the camera follow works,
+        // the x should be close at around the same as y so checking just x should be fine
+        return xOffset < closeEnoguh; // && yOffset < closeEnoguh; 
 
     }
 
