9b1818479ecdb697c47f06b74d0128bf0e3a1a07
Tristan Havelick <tristan@havelick.com>
2022-01-30 17:10:42 -0700

    add GGJ splash screen

--- source/states/SplashScreenState.hx
+++ source/states/SplashScreenState.hx
@@ -32,7 +32,7 @@ class SplashScreenState extends FlxState {
 		// List splash screen image paths here
 		loadSplashImages([
 			new SplashImage(AssetPaths.bitdecaygamesinverted__png),
-			new SplashImage(AssetPaths.ld_logo__png)
+			new SplashImage(AssetPaths.gjj_splash__png)
 		]);
 
 		timer = splashDuration;
